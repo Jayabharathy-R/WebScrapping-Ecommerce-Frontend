86e3bbd5235787e081b007042e11fc87f94c3d6c
Jayabharathy-R <rjbharathy.1991@gmail.com>
2022-05-22 17:04:39 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -17,19 +17,17 @@ import {
 function App(){
   const[data,setData]=useState([]);
 
-  // const[title,setTitle]=useState("");
-  // const[image,setImage]=useState("");
-  // const[mrp,setMrp]=useState(0);
-  // const[price,setPrice]=useState(0);
-  // const[discount,setdiscount]=useState(0);
   const value = 5;
 
   useEffect(()=>{
-     axios.get("http://localhost:3002/WebScraping/getProduct", 
-      { crossdomain: true })
+    //  axios.get("http://localhost:3002/WebScraping/getProduct", 
+    //   { crossdomain: true })
+    fetch("http://localhost:3002/WebScraping/getProduct")
+      .then(res => res.json())
       .then(response => {
 
-      setData(response.data);
+      setData(response);
+      console.log(response)
        // setText(response.data[0].image);
       });
       
@@ -61,164 +59,7 @@ function App(){
               </div>
               </div>
      )})}
-   {/* <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={3} style={{ padding: "50px" }}>
-          <Grid item xs={3}>
-             <Card sx={{ minWidth: 275, height: 400, paddingBottom: "20px" }}>
-              <CardContent>
-                <Typography sx={{ fontSize: 30, height: "100px", backgroundColor: "lightgray", padding: "50px 25px" }} color="text.secondary" gutterBottom>
-                  450 x 300
-                </Typography>
-                <Typography variant="h5" component="div" >
-                  Fancy Product
-                </Typography>
-
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  $40.00 - $80.00
-                </Typography>
-                <br /><br />
-
-              </CardContent>
-             
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-             <Card sx={{ minWidth: 275, height: 400, paddingBottom: "20px" }}>
-              <CardContent>
-                <Typography sx={{ fontSize: 30, height: "100px", backgroundColor: "lightgray", padding: "50px 25px" }} color="text.secondary" gutterBottom>
-                  <span class="sale">sale</span>450 x 300
-                </Typography>
-                <Typography variant="h5" component="div">
-                  Special Item
-                </Typography>
-
-                <Rating name="read-only" value={value} readOnly sx={{ padding: "10px" }} />
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  <del>$20.00</del>&nbsp;$18.00
-                </Typography>
-              </CardContent>
-    
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-            <Card sx={{ minWidth: 275, height: 400, paddingBottom: "20px" }}>
-              <CardContent>
-                <Typography sx={{ fontSize: 30, height: "100px", backgroundColor: "lightgray", padding: "50px 25px" }} color="text.secondary" gutterBottom>
-                  <span class="sale">sale</span>450 x 300
-                </Typography>
-                <Typography variant="h5" component="div">
-                  Sale Item
-                </Typography>
-
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  <del>$50.00</del>&nbsp;$25.00
-                </Typography>
-                <br /><br />
-
-              </CardContent>
-              
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-            <Card sx={{ minWidth: 275, height: 400, paddingBottom: "20px" }}>
-              <CardContent>
-                <Typography sx={{ fontSize: 30, height: "100px", backgroundColor: "lightgray", padding: "50px 25px" }} color="text.secondary" gutterBottom>
-                  450 x 300
-                </Typography>
-                <Typography variant="h5" component="div">
-                  Popular Item
-                </Typography>
-
-                <Rating name="read-only" value={value} readOnly sx={{ padding: "10px" }} />
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  $40.00
-                </Typography>
-
-
-              </CardContent>
-             
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-            <Card sx={{ minWidth: 275, height: 400, paddingBottom: "20px" }}>
-              <CardContent>
-                <Typography sx={{ fontSize: 30, height: "100px", backgroundColor: "lightgray", padding: "50px 25px" }} color="text.secondary" gutterBottom>
-                  <span class="sale">sale</span>450 x 300
-                </Typography>
-                <Typography variant="h5" component="div">
-                  Sale Item
-                </Typography>
-
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  <del>$50.00</del>&nbsp;$25.00
-                </Typography>
-                <br /><br />
-
-              </CardContent>
-              
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-             <Card sx={{ minWidth: 275, height: 400, paddingBottom: "20px" }}>
-              <CardContent>
-
-                <Typography sx={{ fontSize: 30, height: "100px", backgroundColor: "lightgray", padding: "50px 25px" }} color="text.secondary" gutterBottom>
-
-                  450 x 300
-                </Typography>
-                <Typography variant="h5" component="div">
-                  Fancy Product
-                </Typography>
-
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  $40.00 - $80.00
-                </Typography>
-                <br /><br />
-
-              </CardContent>
-              
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-             <Card sx={{ minWidth: 275, height: 400, paddingBottom: "20px" }}>
-              <CardContent>
-                <Typography sx={{ fontSize: 30, height: "100px", backgroundColor: "lightgray", padding: "50px 25px" }} color="text.secondary" gutterBottom>
-                  <span class="sale">sale</span>450 x 300
-                </Typography>
-                <Typography variant="h5" component="div">
-                  Special Item
-                </Typography>
-
-                <Rating name="read-only" value={value} readOnly sx={{ padding: "10px" }} />
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  <del>$20.00</del>&nbsp;$18.00
-                </Typography>
-              </CardContent>
-              
-            </Card>
-          </Grid>
-          <Grid item xs={3}>
-            <Card sx={{ minWidth: 275, height: 400, paddingBottom: "20px" }}>
-              <CardContent>
-                <Typography sx={{ fontSize: 30, height: "100px", backgroundColor: "lightgray", padding: "50px 25px" }} color="text.secondary" gutterBottom>
-                  450 x 300
-                </Typography>
-                <Typography variant="h5" component="div">
-                  Popular Item
-                </Typography>
-
-                <Rating name="read-only" value={value} readOnly sx={{ padding: "10px" }} />
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  $40.00
-                </Typography>
-
-
-              </CardContent>
-              
-            </Card>
-          </Grid>
-        </Grid>
-      </Box> */}
+   
    </div>
    </div>
   );
